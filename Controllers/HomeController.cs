ac3698096b51952acfac35e92d6448c8bd131144
michaelpiscione <piscionemichael@gmail.com>
2022-02-16 13:31:33 -0700

    A lot
    of stuff

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookstore.Models;
+using Bookstore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Bookstore.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private IBookstoreRepository repo;
+
+        public HomeController(IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 5;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                .OrderBy(b => b.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+
+            return View(x);
+        }
+
+    }
+}
---
+++ Models/EFBookstoreRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Bookstore.Models
+{
+    public class EFBookstoreRepository
+    {
+        private BookstoreContext context { get; set; }
+
+        public EFBookstoreRepository(BookstoreContext temp)
+        {
+            context = temp;
+        }
+
+        public IQueryable<Book> Books => context.Books;
+    }
+}
