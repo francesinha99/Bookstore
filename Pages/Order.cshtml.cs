5af75f1527cc7da7f07ae1aaa6ab17b49a308e63
michaelpiscione <piscionemichael@gmail.com>
2022-02-23 21:42:14 -0700

    More stuff
    coool

---
+++ Pages/Order.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bookstore.Pages
+{
+    public class OrderModel : PageModel
+    {
+        private IBookstoreRepository repo { get; set; }
+
+        public OrderModel (IBookstoreRepository temp)
+        {
+            repo = temp; 
+        }
+
+        public Basket basket { get; set; }
+
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost(int bookId)
+        {
+            Book b = repo.Books.FirstOrDefault(x => x.BookId == bookId);
+
+            basket = new Basket();
+
+            basket.AddItem(b, 1);
+
+            return RedirectToPage();
+        }
+    }
+}
