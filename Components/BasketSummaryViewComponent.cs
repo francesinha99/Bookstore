4d6e8524f4c1245bb3f518d270b7d9b9046a3f65
michaelpiscione <piscionemichael@gmail.com>
2022-03-02 21:59:17 -0700

    Finished!
    nice

---
+++ Components/BasketSummaryViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.Components
+{
+    public class BasketSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public BasketSummaryViewComponent(Basket basketService)
+        {
+            basket = basketService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
